b18681d70f4867454f92e86e33e4779e001392a0
Noars <55838787+Noars@users.noreply.github.com>
2021-10-25 16:58:07 +0200

    update langue in url

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivationEnd, Router} from "@angular/router";
+import { filter, map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +13,19 @@ export class LanguageService {
 
   /**
    * @param translate -> Initialize ngx-translate library
-   * @param activatedRoute
+   * @param router
    *
    * Set language to default
    */
-  constructor(private translate: TranslateService, private activatedRoute: ActivatedRoute) {
+  constructor(private translate: TranslateService, private router: Router) {
       this.translate.setDefaultLang(this.startLanguage);
-      this.activeLanguage = location.href.substring(24,26);
+      this.router.events.pipe(
+        filter(e => (e instanceof ActivationEnd) && (Object.keys(e.snapshot.params).length > 0)),
+        map(e => e instanceof ActivationEnd ? e.snapshot.params : {})
+      ).subscribe(params => {
+        console.log(Object.values(params)[0]);
+        this.activeLanguage = Object.values(params)[0];
+      });
       setTimeout(() => {
         this.switchLanguage();
       }, 300);
